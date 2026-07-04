87b33008b9d3dd087307934cbd34cbfe5950d320
JakubJarosz <jakubtm165@gmail.com>
2026-07-04 18:38:53 +0200

    Update EnemyVisualEvents.cs

--- Assets/Scripts/Enemy/EnemyVisualEvents.cs
+++ Assets/Scripts/Enemy/EnemyVisualEvents.cs
@@ -3,8 +3,13 @@ using UnityEngine;
 public class EnemyVisualEvents : MonoBehaviour
 {
     [SerializeField] private ProjectileSpawn projectile;
+    [SerializeField] private EnemyMeleeHitBox meleeHitBox;
 
     public void SpawnProjectile() {
         projectile.SpawnProjectile();
+    }
+
+    public void MeleeHit() {
+        meleeHitBox.MeleeHit();
     }
 }
